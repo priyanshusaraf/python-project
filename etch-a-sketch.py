6937800753082929514a5b82272eb13798591ea3
priyanshusaraf <66620510+priyanshusaraf@users.noreply.github.com>
2020-11-28 20:24:36 +0530

    Update etch-a-sketch.py

--- etch-a-sketch.py
+++ etch-a-sketch.py
@@ -1,19 +1,40 @@
 import turtle
+
 win = turtle.Screen()
 turt = turtle.Turtle()
 
 def forward():
   turt.forward(10)
+
+  
 def backward():
   turt.backward(10)
+
+  
 def left():
   turt.left(10)
+
+  
 def right():
   turt.right(10)
 
+  
+def undo():
+    turt.undo()
+
+    
+def clear():
+    turt.clear()
+    turt.penup()
+    turt.home()
+    turt.pendown()
+
+
 win.listen()
-win.onkey("w", forward)
-win.onkey("a", backward)
-win.onkey("s", left)
-win.onkey("d", right)
+win.onkey(key="w", fun=forward)
+win.onkey(key="s", fun=backward)
+win.onkey(key="a", fun=left)
+win.onkey(key="d", fun=right)
+win.onkey(key="u", fun=undo)
+win.onkey(key="c", fun=clear)
 win.exitonclick()
